054029166a4477e63d91170a6b3a4591ee148cdf
pilif01 <bulcfilip641@gmail.com>
2025-03-25 00:35:30 +0200

    update

--- src/main/java/org/example/DBConnection.java
+++ src/main/java/org/example/DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/login_system";
-    private static final String USER = "loginuser";  // Use "root" if no user was created
-    private static final String PASSWORD = "password123";  // Change this to your MySQL password
+    private static final String USER = "loginuser";
+    private static final String PASSWORD = "password123";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
